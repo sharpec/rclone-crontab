f576a3051fa080d15222c73574f7ce2b434a68c1
Enzo <sharpec@gmail.com>
2021-04-17 14:35:21 +0200

    fix log and notification

--- rclone-crontab.sh
+++ rclone-crontab.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+!/bin/bash
 set -e
 if [ ! $# -eq 1 ]
 then
@@ -9,14 +9,14 @@ then
 else
         FOLDER=$1
 fi
-#echo $FOLDER
+echo $FOLDER
 SCRIPTDIR="/etc/rclone-crontab"
 SADIR="$SCRIPTDIR/sa"
 LOGDIR="/var/log/rclone"
 DATETIME="`date +%Y-%m-%d_%H-%M-%S`"
-LOGFILE="$LOGDIR/$FOLDER-$DATETIME.log"
-LOGCHECK="$LOGDIR/check-$FOLDER-$DATETIME.txt"
-LOGDEDUP="$LOGDIR/dedupe-$FOLDER-$DATETIME.txt"
+LOGFILE="$LOGDIR/$FOLDER-$DATETIME.txt"
+LOGCHECK="$LOGDIR/$FOLDER-$DATETIME-check.txt"
+LOGDEDUP="$LOGDIR/$FOLDER-$DATETIME-dedup.txt"
 OPTION="
   --fast-list \
   --transfers=16 \
@@ -38,12 +38,14 @@ OPTION="
 #   --track-renames \
 mkdir -p $LOGDIR
 mkdir -p $SADIR
+echo "Running rclone dedupe:"
 #
-# Check duplicated file
+# Chekc duplicated file
 rclone dedupe "td-$FOLDER-bck:0/" \
         --drive-service-account-file="$SADIR/$FOLDER.json" \
         --dedupe-mode oldest \
         2>&1 | tee $LOGDEDUP
+#read -p "Press any key to resume ..."
 #
 # Running Rclone
 echo
@@ -54,22 +56,24 @@ rclone $OPTION sync "td-$FOLDER:" "td-$FOLDER-bck:0/" \
         --stats-log-level NOTICE \
         --drive-service-account-file="$SADIR/$FOLDER.json" \
         2>&1 | tee $LOGFILE
-#
-# Running check and alert
 echo "Running rclone check:"
 rclone check "td-$FOLDER:" "td-$FOLDER-bck:0/" \
         --drive-service-account-file="$SADIR/$FOLDER.json" \
         --log-level=ERROR \
         2>&1 | tee $LOGCHECK
-        
-if [ -s "$LOGCHECK" ] 
+echo "Validate backup and notify"
+if [ ! -s $LOGCHECK ]
 then
+        echo "Backup Successful"
+        apprise -vv -t ""$FOLDER" OK" \
+        -b "Backup "$FOLDER" is perfect" \
+        --config=$SCRIPTDIR/apprise.conf
+else
         echo "Backup with error"
         apprise -vv -t ""$FOLDER" error" \
         -b "Backup "$FOLDER" with error" \
-        --attach $LOGDEDUP
-        --attach $LOGFILE
+        --config=$SCRIPTDIR/apprise.conf \
+        --attach $LOGDEDUP \
+        --attach $LOGFILE \
         --attach $LOGCHECK
-else
-        echo "Backup Successful"
 fi
